101e02770d551a8ed6efee83653fbc69dc9028ce
Sara <mublasa@gmail.com>
2020-06-10 20:54:55 +0200

    Order added to DB

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class OrderController extends Controller
 {
@@ -29,9 +31,20 @@ class OrderController extends Controller
 
     public function storeOrder(Request $request)
     {
-        // Almacenar ordene en DB
-        // Retrieve latest id registered as ltId
-        // $ltId = ;
+        $i = 0;
+        $ltId = 1;
+        $burgers = session()->get('myOrder');
+
+        do{
+            $newOrder = new Order();
+            $newOrder->user_id = Auth::user()->id;
+            $newOrder->product_id = $burgers[$i]->product_id;
+            $newOrder->amount = $burgers[$i]->amount;
+
+            $newOrder->save();
+
+            $i++;
+        }while($i < sizeof($burgers));
         
         session()->forget('myOrder');
         session()->put("info", 'Order succesfully placed. It will be ready in 10 min.');
